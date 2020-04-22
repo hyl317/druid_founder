797022914121aef9c0b7ed042024dee45f6f6aaa
hyl317 <yh362@cornell.edu>
2020-04-22 16:17:03 -0400

    debug IBD sharing plot

--- ibd_share_distribution.py
+++ ibd_share_distribution.py
@@ -2,15 +2,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import gzip
+import argparse
 from collections import defaultdict
 
 def readIBD(ibdFile):
-    ibd_length = defaultdict(0)
+    ibd_length = defaultdict(lambda:0)
     with gzip.open(ibdFile, 'rt') as f:
         line = f.readline()
         while line:
             ind1, hap1, ind2, hap2, chr, start_bp, end_bp, len_cM = line.strip().split('\t')
-            ind1, ind2 = sorted(ind1, ind2)
+            ind1, ind2 = sorted([ind1, ind2])
             pair = (ind1, ind2)
             ibd_length[pair] += float(len_cM)
             line = f.readline()
@@ -24,8 +25,14 @@ def main():
     args = parser.parse_args()
 
     lens = np.array([value for key, value in readIBD(args.ibd).items()])
-    plt.hist(lens, weights=np.zeros_like(lens)+1/lens.size)
+    print(lens.size)
+    print(f'mean is {np.mean(lens)} and std is {np.std(lens)}')
+    mu, std = np.mean(lens), np.std(lens)
+    lens = lens[lens <= mu+3*std]
+    plt.hist(lens, weights=np.zeros_like(lens)+1/lens.size, bins=100)
     plt.xlabel('total IBD Sharing Length per pair of diploid individuals')
-    plt.save('ibdSharing.png', dpi=300)
+    plt.savefig('ibdSharing.png', dpi=300)
 
 
+if __name__ == '__main__':
+    main()
