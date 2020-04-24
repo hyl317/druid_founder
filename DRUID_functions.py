50b6528a474b77fd4e8666d361df518578a16ab0
Yilei Huang <yh362@cornell.edu>
2020-04-24 09:19:59 -0400

    another fix

--- DRUID_functions.py
+++ DRUID_functions.py
@@ -800,7 +800,7 @@ def combineBothGPsKeepProportionOnlyExpectation(sib1, avunc1, pc1, sib2, avunc2,
     #    print(f'{sib1}, {avunc1}')
     #    print(f'{sib2},{avunc2}')
 
-    print(f'num_R1\t{num_R1}\tnum_R2\t{num_R2}')
+    print(f'num_R1\t{sib1_len+sib2_len}\tnum_R2\t{av1_len+av2_len}')
     print(f'total IBD before correction: {tmp}')
     print(f'total IBD after correction: {tmpsibav}')
     #MY MODIFICATION ENDS HERE
