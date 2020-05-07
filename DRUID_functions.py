a159f5742d01662273bd3300b5d8c48094a19465
hyl317 <yh362@cornell.edu>
2020-05-07 01:54:03 -0400

    something new

--- DRUID_functions.py
+++ DRUID_functions.py
@@ -1164,17 +1164,17 @@ def getAllRel(results_file, inds_file, hapibd_segs, C, useK):
                     alter_lik = max(alter_lik, null_lik)
                     chi2 = -2*(null_lik - alter_lik)
                     p_value = 1 - scipy.stats.chi2.cdf(chi2, df=2)
-                    #print(f'{ind1}, {ind2}')
+                    print(f'{ind1}, {ind2}')
                     #print(f'number of ibd segments: {len(ibd_list)}')
                     #print(ibd_list)
                     #print(f'null likelihood: {null_lik}')
                     #print(f'alternative likelihood: {alter_lik}')
-                    #print(f'degree estimated from K: {degree}', flush=True)
+                    print(f'degree estimated from K: {degree}', flush=True)
                     if p_value < 0.01:
                         degree = d
                     else:
                         degree = -1
-                    #print(f'degree estimated from ERSA-like approach: {degree}, a={a}, n_p={n_p}', flush=True)
+                    print(f'degree estimated from ERSA-like approach: {degree}, a={a}, n_p={n_p}', flush=True)
 
             all_rel[ind1][ind2] = [ibd1,ibd2, K, degree]
 
